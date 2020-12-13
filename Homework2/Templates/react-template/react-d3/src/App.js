7f451146db0ca0b7c3a47fc50c6cc191d4d7bdd3
Keshav Dasu <keshavdasu@gmail.com>
2020-12-13 04:45:17 -0800

    React Example with Dataset

--- Homework2/Templates/react-template/react-d3/src/App.js
+++ Homework2/Templates/react-template/react-d3/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import BarChart from './BarChart';
+import data from  './datasets/SF_Historical_Ballot_Measures.csv';
 import './App.css';
 
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state ={
-      data: [12, 5, 6, 6, 9, 10],
-      width: 700,
-      height: 500,
-      id: 'root'
+      data: data,
+      id: "chart-1"
     }
   }
 
   render(){
     return (
       <div className="App">
+        <h2>ECS 272 Assignment 3 D3 Template</h2>
+        <div id="container"> 
+        <div id="tooltip"></div>
+        </div>
         <BarChart data={this.state.data} width={this.state.width} height={this.state.height} />
       </div>
     );
