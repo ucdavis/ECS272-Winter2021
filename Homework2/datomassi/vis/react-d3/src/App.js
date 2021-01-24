04379ea11a7a7766dc9bc52f26036db706881a7f
dtomassi <datomassi@ucdavis.edu>
2021-01-24 13:52:30 -0800

    updates

--- Homework2/datomassi/vis/react-d3/src/App.js
+++ Homework2/datomassi/vis/react-d3/src/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import BarChart from './BarChart';
+import SpiderPlot from './SpiderPlot';
 import ScatterPlot from './ScatterPlot';
-// import data from  './datasets/SF_Historical_Ballot_Measures.csv';
-import data from './datasets/data_by_year.csv';
+import dataGenre from  './datasets/data_by_genres.csv';
+import dataYear from './datasets/data_by_year.csv';
 import './App.css';
 
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state ={
-      data: data,
+      dataYear: dataYear,
+      dataGenre: dataGenre,
       id: "chart-1"
     }
   }
+
+
 
   render(){
     return (
       <div className="App">
         <h2>ECS 272 Assignment 3 D3 Template</h2>
         <div id="container"> 
-        <div id="tooltip"></div>
+        <div id="tooltip">asdasdssss</div>
         </div>
-        <ScatterPlot data={this.state.data} width={this.state.width} height={this.state.height} />
+        <ScatterPlot data={this.state.dataYear} width={this.state.width} height={this.state.height} />
+        <SpiderPlot data={this.state.dataGenre} />
       </div
  >   );
   }
