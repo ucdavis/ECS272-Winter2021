bd4f8cacd0f174d37db79154ffbc9299ac175de1
Zonks <Jeremy.tj.lin@gmail.com>
2021-02-02 23:44:26 -0800

    Incomplete - Final commit before submission

--- Homework2/jtjlin/src/BrushChart.js
+++ Homework2/jtjlin/src/BrushChart.js
@@ -1,58 +1,155 @@
-import React, {useRef, useEffect} from "react";
+import React, {useRef, useEffect, useState} from "react";
 import * as d3 from "d3";
-import {select, line} from "d3";
+import {select, line, curveCardinal, axisBottom, axisLeft} from "d3";
 import useResizeObserver from "./useResizeObserver";
 import GroupBy from "./GroupBy";
+import CheckPreviousState from "./CheckPreviousState.js";
 
-/* code adapted from "The Muratorium" d3 react Youtube tutorials v=bPNkdoEqfVY */
+/* code adapted from "The Muratorium" d3 react Youtube tutorials */
+
 
 function BrushChart({data}){
     const svgRef = useRef(); //reference object hook that can be passed to d3
     const wrapperRef = useRef();
     const dimensions = useResizeObserver(wrapperRef);
-
-    d3.csv(data)
-    .then(csv => {
-        // create data by selecting two columns from csv 
-        var data = csv.map(row => {
-            return {
-              xCoord: Number(row['X']),
-              yCoord: Number(row['Y']),
-              district: row['PdDistrict'],
-              Category: row['Category'],
-              cDate: row['Date']
-            }
-        })
-        let sortedata = GroupBy(data,"district");
-        console.log(sortedata);
-    })
-
-
+    const [selection, setSelection] = useState([1,2]) // default selection of brush
+    const previousSelection = CheckPreviousState(selection);
+    
+    var dateParse = d3.timeParse("%m/%d/%Y %H:%M:%S %p");
+ 
     useEffect(() => { //hook that gets triggered everytime elements change 
-        const svg = select(svgRef.current); //pass svg element to d3
-        
-        const myLine = line() //where to render each dot on line
-         .x((value, index) => index  *50)
-         .y(value => value);
+        //data filtering
+        d3.csv(data)
+        .then(csv => {
+            // create data by selecting two columns from csv 
+            var filterdata = csv.map(row => {
+                return {
+                xCoord: Number(row['X']),
+                yCoord: Number(row['Y']),
+                district: row['PdDistrict'],
+                Category: row['Category'],
+                cDate: dateParse(row['Date']),
+                cMonth: dateParse(row['Date']).getMonth()+1
+                }
+            })
+            //console.log(csv);
+            const sortedata = GroupBy(filterdata,"cMonth");
+            //console.log(sortedata);
+            //console.log(Object.values(sortedata));
 
-        //attach d attribute to each path element, need to wrap data array in [] because want 1 path for whole array
-        svg.selectAll("path")
-         .data([data])
-         .join("path")
-         .attr("d", value => myLine(value))
-         .attr("fill", "none")
-         .attr("stroke", "blue");
+            const monthNames = ["January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November", "December"
+                        ];
 
-        //use dimensions otherwise fallback to boundingClientRect dom element
-        const{width, height} = 
-            dimensions || wrapperRef.current.getBoundingClientRect();
+            var plotdata = Object.values(sortedata).map((row, index) => {
+                return {
+                    Month: Number(index+1),
+                    Monthname: monthNames[index],
+                    cSum: row.length
+                }
+            })
+            //console.log(plotdata);
 
-    }, [data]); //dependencies
+            //if have more time add more points - get actual dates
+
+            //use dimensions otherwise fallback to boundingClientRect dom element
+            const{width, height} = 
+            dimensions || wrapperRef.current.getBoundingClientRect();   
+
+            const svg = select(svgRef.current); //pass svg element to d3
+
+            //setup scales - transforms input value into visual representation
+            // const xScale = d3.scaleBand()
+            //  .domain(plotdata.map(d => d.Monthname))
+            //  .rangeRound([0, width]);
+
+            const xScale = d3.scaleLinear()
+             .domain([d3.min(plotdata, d => d.Month),d3.max(plotdata, d => d.Month)])
+             .rangeRound([0, width]);
+
+            const yScale = d3.scaleLinear()
+             .domain([d3.min(plotdata, d => d.cSum)-100,d3.max(plotdata, d => d.cSum)+100])
+             .range([height, 0]);
+
+
+            var lineGenerator = line() //where to render each dot on line
+             .x(d => xScale(d.Month)+55) //offset by 55 why?
+             .y(d => yScale(d.cSum))
+             .curve(curveCardinal);
+
+            // svg.append("path")
+            //  .datum(plotdata) // 10. Binds data to the line 
+            //  .attr("fill", "none")
+            //  .attr("stroke", "steelblue")
+            //  .attr("stroke-width", 1.5)
+            //  .attr("stroke-linejoin", "round")
+            //  .attr("stroke-linecap", "round")
+            //  .attr("d", myLine); // 11. Calls the line generator             
+
+            //attach d attribute to each path element, need to wrap data array in [] because want 1 path for whole array
+            svg.selectAll(".myLine") //synch with all data elements
+             .data([plotdata])
+             //.datum(plotdata)
+             .join("path")
+             .attr("class", "myLine")
+             .attr("stroke", "steelblue")
+             .attr("stroke-width", 1.5)
+             .attr("stroke-linejoin", "round")
+             .attr("stroke-linecap", "round")
+             .attr("fill", "none")
+             .attr("d", lineGenerator); // call line generator
+
+
+            svg.selectAll(".myDot") //Selects all defined elements in the DOM and hands off a reference to the next step in the chain.
+             .data(plotdata) //connect chart data with DOM <rect/> elements
+             .join("circle") // appends a new SVG rect element for each element in our chartdata array.
+             .attr("class", "myDot")
+             .attr('r', (value, index) => index >= selection[0] && index <= selection[1] ? 4 : 2)
+             .attr('fill', (value, index) => index >= selection[0] && index <= selection[1] ? "orange" : "blue")
+             .attr('cy', (d) => yScale(d.cSum)) //since this is a vertical bar chart we start the bottom of the bars at the bottom of the chart
+             .attr('cx', (d) => xScale(d.Month)+55) // bar x position 
+             .attr("fill","blue"); // color of the bar
+
+            // x and y axis
+            const xAxis = axisBottom(xScale); // call xaxis function with current svg selection
+            svg.select(".x-axis")
+            .style("transform", `translateY(750px)`)
+            .call(xAxis);
+
+            const yAxis = axisLeft(yScale);
+            svg.select(".y-axis")
+            .style("transform", "translateX(0px)")
+            .call(yAxis);
+                     
+            //brush along x-axis
+            const brush = d3.brushX()
+             .extent([[0,0], [width, height]]) // brush can move from top left corner to bottom right corner of svg (whole area)
+             .on("start brush end", (event) => {
+                 if(event.selection){
+                    const indexSelection = event.selection.map(xScale.invert) //selection of the brush in event object from d3 using d3 inverting pixel value back to index
+                    setSelection(indexSelection);
+                }
+                 
+
+             })
+
+             if(previousSelection === selection){ //if previous selection is the same as current one
+                svg.select(".brush")
+                .call(brush);
+                //.call(brush.move, selection.map(xScale)); //starting default position and selection
+             }
+            
+        })
+    }, [data, dateParse, dimensions]); //dependencies
 
     return (
         //reference wrapperRef for dimensions of div, svg-dom element
         <div ref={wrapperRef} style={{ marginBottom: "2rem"}}> 
-            <svg ref={svgRef}></svg> 
+            <svg ref={svgRef}>
+                <g className="x-axis" />
+                <g className="y-axis" /> 
+                <g className="brush" />  
+            </svg> 
         </div>
     );
 
--- Homework2/jtjlin/src/GeoChart.js
+++ Homework2/jtjlin/src/GeoChart.js
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import {select, geoPath, geoMercator, min, max} from "d3";
 import useResizeObserver from "./useResizeObserver";
 import GroupBy from "./GroupBy";
@@ -22,48 +22,122 @@ function GeoChart({mapData, pdData}){
     const svgRef = useRef();
     const wrapperRef = useRef();
     const dimensions = useResizeObserver(wrapperRef);
+    const [selectedDistrict, setSelectedDistrict] = useState(null);
     //console.log(data);
+    var dateParse = d3.timeParse("%m/%d/%Y %H:%M:%S %p");
 
-
-    d3.csv(pdData)
-    .then(csv => {
-        // create data by selecting two columns from csv 
-        var data = csv.map(row => {
-            return {
-              xCoord: Number(row['X']),
-              yCoord: Number(row['Y']),
-              district: row['PdDistrict'],
-              Category: row['Category'],
-              cDate: row['Date']
-            }
-        })
-        let sortedata = GroupBy(data,"district");
-        console.log(sortedata);
-    })
+    // let temp1 = dateParse('01/29/2017 12:00:00 AM');
+    // console.log(temp1);
+    // console.log(temp1.getMonth()+1,temp1.getYear()+1900,temp1.getHours());
     
 
     //called initially and then for each data update
     useEffect(() => {
         const svg = select(svgRef.current);
-        
 
-        //let groupedData = GroupBy(d3.csv(pdData),"Pd District");
-        //console.log(groupedData);
+        //data filtering
+        d3.csv(pdData)
+        .then(csv => {
+          // create data by selecting two columns from csv 
+          var filterdata = csv.map(row => {
+              return {
+              xCoord: Number(row['X']),
+              yCoord: Number(row['Y']),
+              district: row['PdDistrict'],
+              Category: row['Category'],
+              cDate: dateParse(row['Date']),
+              cMonth: dateParse(row['Date']).getMonth()+1
+              }
+          })
 
-        //use dimensions otherwise fallback to boundingClientRect dom element
-        const{width, height} = 
+          filterdata.sort(function(a,b){
+            // Turn your strings into dates, and then subtract them
+            // to get a value that is either negative, positive, or zero.
+            return (new Date(a.cDate) - new Date(b.cDate));
+          });
+          //console.log(filterdata);
+
+          const sortedata = GroupBy(filterdata,"district");
+          //console.log(sortedata);
+          //console.log(Object.values(sortedata));
+
+          var plotdata = Object.values(sortedata).map((row,index) => {
+              return {
+                  District: Object.keys(sortedata)[index],
+                  TotCrime: row.length
+              }
+          })
+          plotdata.splice(-1,1); // get rid of last array entry (extra)
+          //console.log(plotdata);
+
+          //use dimensions otherwise fallback to boundingClientRect dom element
+          const{width, height} = 
             dimensions || wrapperRef.current.getBoundingClientRect();
 
-        const projection = geoMercator().fitSize([width, height], mapData); //project to 2d plane using geoMercator function
-        const pathGenerator = geoPath().projection(projection) //path generator that the function returns, transform json to d attribute of path element
-    
-        svg.selectAll(".district") //select all existing elements with class name neighborhood/district
-         .data(mapData.features) //map to Features array in json
-         .join("path") //draw a path for every new piece of data
-         .attr("class", "district") //attach class name neighborhood to each
-         .attr("d", feature => pathGenerator(feature)); //define callback function that receives current feature from features array and pass to path gen and return appropriate d
-    
-    }, [mapData, pdData, dimensions]); //dependencies
+
+          /* csv and code snippet from https://web.stanford.edu/class/archive/cs/cs448b/cs448b.1166/cgi-bin/wiki/index.php?title=Assignment_3:_Creating_Interactive_Visualization_Software */
+          // // Set up projection that map is using
+          // var projection = geoMercator()
+          //     .center([-122.433701, 37.767683]) // San Francisco, roughly
+          //     .scale(225000)
+          //     .translate([width / 2, height / 2]);
+
+          // // This is the mapping between <longitude, latitude> position to <x, y> pixel position on the map
+          // // projection([lon, lat]) returns [x, y]
+          // // projection.invert([x, y]) returns [lon, lat]
+
+          // // Add svg map at correct size, assumes map is saved in a subdirectory called "data"
+          // svg.append("image")
+          //   .attr("width", width)
+          //   .attr("height", height)
+          //   .attr("xlink:href", "datasets/sf-map.svg");
+          /********************************************************************************************************************* */
+
+          //console.log(plotdata.map (function(e) {  return e.TotCrime;}));
+          const minProp = min(plotdata.map (function(e) {  return e.TotCrime;}));
+          const maxProp = max(plotdata.map (function(e) {  return e.TotCrime;}));
+          console.log(plotdata)
+          console.log(minProp,maxProp);
+          const colorScale = d3.scaleLinear()
+            .domain([minProp,maxProp])
+            .range("#ccc","red")
+
+          const projection = geoMercator().fitSize([width, height], mapData).precision(100); //project to 2d plane using geoMercator function
+          const pathGenerator = geoPath().projection(projection) //path generator that the function returns, transform json to d attribute of path element
+      
+          svg.selectAll(".district") //select all existing elements with class name neighborhood/district
+          .data(mapData.features) //map to Features array in json
+          .join("path") //draw a path for every new piece of data
+          .on("click", feature =>{
+              setSelectedDistrict(selectedDistrict === feature ? null : feature); //if selected district is already selected set selected to null.
+          })
+          .attr("class", "district") //attach class name neighborhood to each
+          .transition()
+          //.attr("fill", "#ccc")
+          .attr("fill",  (d) =>colorScale(d.TotCrime))
+          .attr("d", feature => pathGenerator(feature)); //define callback function that receives current feature from features array and pass to path gen and return appropriate d
+
+          //render text element of district name and stats
+          svg.selectAll(".label")
+            //.data([selectedDistrict])
+            .data(plotdata)
+            .join("text")
+            .attr("class", "label")
+            //.text(feature => feature.District.toLocaleString())
+            .attr("x", 10)
+            .attr("y", 25);
+
+          // // add circles to svg
+          // svg.selectAll("circle")
+          //  .data([filterdata.xCoord,filterdata.yCoord]).enter //UNDEFINED DATA...
+          //  .append("circle")
+          //  .attr("cx", function (d) { console.log(projection(d)); return projection(d)[0]; })
+          //  .attr("cy", function (d) { return projection(d)[1]; })
+          //  .attr("r", "8px")
+          //  .attr("fill", "red");
+            
+        });
+    }, [mapData, pdData, dimensions, dateParse, selectedDistrict]); //dependencies
 
     return (
         //reference wrapperRef for dimensions of div
