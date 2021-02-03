fa263d58eb7c342bf0f958409a27b356c77fbc10
cafornaca <cafornaca@yahoo.com>
2021-02-03 13:39:16 -0800

    Adding datasets for hw2 back oops

---
+++ Homework3/cafornaca/main.js
@@ -0,0 +1,5 @@
+ // Load the dataset from a CSV URL
+ d3.csv('https://raw.githubusercontent.com/cafornaca/ECS272-Winter2021/main/Homework2/datasets/Police_Department_Incidents_-_Previous_Year__2016_.csv')
+ .then(csv => {
+     
+ })
