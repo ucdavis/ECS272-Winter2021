a762cd78fc343268675946eb80f3f0f00e4d1b1a
kunhuang <kunhuang@ucdavis.edu>
2021-03-14 06:01:28 -0700

    finished all integration

--- FinalProject/react-template/react-d3/src/ParallelCoordinate.js
+++ FinalProject/react-template/react-d3/src/ParallelCoordinate.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./ParallelCoordinate.css";
-
+import BarChart4 from "./BarChart4";
 
 class ParallelCoordinate extends React.Component{
   constructor(props){
@@ -17,8 +17,9 @@ class ParallelCoordinate extends React.Component{
     <div className="PCpageLayout">
       <h1 className="header">Parallel Coordinate</h1>
       <div className="PCcontainer">
+      <a className="google_map_home" href="/geoOverview">Map</a>
         <div className="PC">
-
+          <BarChart4/>
         </div>
       </div>
     </div>
