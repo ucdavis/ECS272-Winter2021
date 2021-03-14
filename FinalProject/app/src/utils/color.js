b8b1cc8b71eb9396b4b6e9264c2c3a0b1a3f333a
fangzhouli <fzli0805@gmail.com>
2021-03-14 14:22:31 -0700

    improved color function, colorByGenre and colorByRating.

--- FinalProject/app/src/utils/color.js
+++ FinalProject/app/src/utils/color.js
@@ -4,7 +4,17 @@ import * as d3 from 'd3';
  * Input: Int between 0 and 19.
  * Output: A color, e.g. `attr('fill', color[0])``
  */
-const color = d3.scaleOrdinal()
+export const colorByRating = d3.scaleOrdinal()
+  .domain(['G', 'PG', 'PG-13', 'R'])
+  .range(['#c7c7c7',
+          '#bcbd22',
+          '#dbdb8d',
+          '#17becf']);
+
+export const colorByGenre = d3.scaleOrdinal()
+  .domain(['Action', 'Adult', 'Adventure', 'Comedy', 'Drama', 'Fantasy',
+           'Kids', 'Romance', 'School', 'Sci-Fi', 'Seinen', 'Shounen',
+           'Slice of Life', 'Supernatural', 'Other'])
   .range(['#1f77b4',
           '#aec7e8',
           '#ff7f0e',
@@ -19,11 +29,4 @@ const color = d3.scaleOrdinal()
           '#c49c94',
           '#e377c2',
           '#f7b6d2',
-          '#7f7f7f',
-          '#c7c7c7',
-          '#bcbd22',
-          '#dbdb8d',
-          '#17becf',
-          '#9edae5']);
-
-export default color;
+          '#7f7f7f']);
