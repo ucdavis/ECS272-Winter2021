d66362c7aa602d01091b86917e99d79666ac0e5c
fangzhouli <fzli0805@gmail.com>
2021-03-14 14:24:37 -0700

    added website title

--- FinalProject/app/src/App.js
+++ FinalProject/app/src/App.js
@@ -5,6 +5,7 @@ import StreamGraph from './components/stream-graph/StreamGraph';
 function App() {
   return (
     <div className='App'>
+      <h3>What Kinds of Animes People Like to Watch?</h3>
       <div id='container'>
         <StreamGraph />
       </div>
