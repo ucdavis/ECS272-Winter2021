380388165d0c5d58202411df99afa94f9fcb6cb7
The-Boogeyman <aoblebea@ucdavis.edu>
2021-03-14 20:08:01 -0700

    Final

---
+++ FinalProject/es6-webpack-template/node/src/index.js
@@ -0,0 +1,7 @@
+
+import testData from "./data/test.json"; /* Example of reading in data */
+import './css/style.scss';
+import './css/styles.css'; /* Example of connecting a style-sheet */
+import {drawMap} from "./js/map";
+
+drawMap();
