2c35563779cd09a44ffca8f6638885aec69b6ffd
Zhennan Wu <znwu@ucdavis.edu>
2021-03-14 22:22:24 -0700

    add slides

--- FinalProject/znwu/src/App.tsx
+++ FinalProject/znwu/src/App.tsx
@@ -9,6 +9,17 @@ import { data, DataEntry } from "./data";
 import usePromise from "react-use-promise";
 import { SidePanel } from "./components/side_panel/side_panel";
 import { Overview } from "./components/overview/overview";
+import "react-slideshow-image/dist/styles.css";
+import { Fade } from "react-slideshow-image";
+
+import img1 from "./images/DM-coronavirus.jpg";
+import img2 from "./images/1fbefc837b2407dd6782f10f0cba0de6aa8f7246-1000x600.png";
+import img3 from "./images/Global COVID-19 Cases Per Capita.webp";
+import img4 from "./images/good-vs-bad-food-unhealthy-junk-iStock_000058524328_Medium.jpg";
+import img5 from "./images/EUS9VAaWsAMug35.jpg";
+import img6 from "./images/obesity-may-increase-risk-of-covid-19-complications-722x406.jpg";
+import img7 from "./images/mark-fletcher-brown-nN5L5GXKFz8-unsplash.jpg"
+import { icons } from "./icons/icons";
 
 const { Title, Text } = Typography;
 
@@ -27,8 +38,180 @@ const tipFormatter = (pos?: number) =>
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Overview />
+    <div className="slide-container App">
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="//fonts.googleapis.com/css?family=Raleway"
+      />
+      <Fade autoplay={false}>
+        <div className="each-fade">
+          <h2>Covid and Healthy Diet</h2>
+        </div>
+        <div className="each-fade">
+          <h2>Covid Situations</h2>
+          <div className="info">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <img src={img1}></img>
+                  </td>
+                  <td>
+                    <img src={img2}></img>
+                  </td>
+                  <td>
+                    <img src={img3}></img>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Covid strikes</td>
+                  <td>Protection, Distancing</td>
+                  <td>Global Pandemic</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="each-fade">
+          <h2>Covid and Diet</h2>
+          <div className="info">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <img src={img4}></img>
+                  </td>
+                  <td>
+                    <img src={img5}></img>
+                  </td>
+                  <td>
+                    <img src={img6}></img>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Covid Susceptibility and Diet Habit</td>
+                  <td>Alcohol Intake</td>
+                  <td>Obesity</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="each-fade">
+          <h2>Food Category</h2>
+          <div className="foodtype">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <img src={icons.Cereals}></img>
+                  </td>
+                  <td>
+                    <img src={icons.Vegetables}></img>
+                  </td>
+                  <td>
+                    <img src={icons.Fruits}></img>
+                  </td>
+                  <td>
+                    <img src={icons.Milk}></img>
+                  </td>
+                  <td>
+                    <img src={icons.Eggs}></img>
+                  </td>
+                  <td>
+                    <img src={icons.StarchyRoots}></img>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Cereals</h4>
+                  </td>
+                  <td>
+                    <h4>Vegetables</h4>
+                  </td>
+                  <td>
+                    <h4>Fruits</h4>
+                  </td>
+                  <td>
+                    <h4>Milk</h4>
+                  </td>
+                  <td>
+                    <h4>Eggs</h4>
+                  </td>
+                  <td>
+                    <h4>Starchy Roots</h4>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img src={icons.Animal}></img>
+                  </td>
+                  <td>
+                    <img src={icons.FishSeafood}></img>
+                  </td>
+                  <td>
+                    <img src={icons.Sugar}></img>
+                  </td>
+                  <td>
+                    <img src={icons.Alcohol}></img>
+                  </td>
+                  <td>
+                    <img src={icons.Oil}></img>
+                  </td>
+                  <td>
+                    <img src={icons.Nuts}></img>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Meat &amp; Animal Products</h4>
+                  </td>
+                  <td>
+                    <h4>Fish &amp; Seafood</h4>
+                  </td>
+                  <td>
+                    <h4>Sugar</h4>
+                  </td>
+                  <td>
+                    <h4>Alcohol Beverages</h4>
+                  </td>
+                  <td>
+                    <h4>Oil</h4>
+                  </td>
+                  <td>
+                    <h4>Nuts</h4>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="each-fade">
+          <Overview />
+        </div>
+        <div className="each-fade">
+          <h2>Conclusion</h2>
+          <div className="info">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <img src={img4}></img>
+                  </td>
+                  <td>
+                    <img src={img7}></img>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Diets do affect COVID susceptibility...</td>
+                  <td>Feel free to explore new trends...</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </Fade>
     </div>
   );
 };
